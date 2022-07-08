d1fbe3d36a74f13a180c0b594072f14b04397a13
Dominick Peluso <peluso.dominick@gmail.com>
2022-07-08 15:21:16 -0400

    Fix footer

--- islands/Footer.tsx
+++ islands/Footer.tsx
@@ -5,16 +5,22 @@ export default function Footer() {
   return (
     <div id="footer">
       {/* https://icon-sets.iconify.design/?query=github */}
-      <span class="iconify" data-icon="simple-icons:deno"></span>{" "}
-      <a href="https://deno.com/deploy" target="_blank">
-        Hosted by Deno Deploy
-      </a>
-      <span class="iconify" data-icon="fa-solid:lemon"></span>
-      <a href="https://fresh.deno.dev/" target="_blank">Built with Fresh</a>
-      <span class="iconify" data-icon="simple-icons:github"></span>{" "}
-      <a href="https://github.com/dominickp/dom-deno" target="_blank">
-        Source on GitHub
-      </a>
+      <span class="inline-block">
+        <span class="iconify" data-icon="simple-icons:deno"></span>{" "}
+        <a href="https://deno.com/deploy" target="_blank">
+          Hosted by Deno Deploy
+        </a>
+      </span>
+      <span class="inline-block">
+        <span class="iconify" data-icon="fa-solid:lemon"></span>
+        <a href="https://fresh.deno.dev/" target="_blank">Built with Fresh</a>
+      </span>
+      <span class="inline-block">
+        <span class="iconify" data-icon="simple-icons:github"></span>{" "}
+        <a href="https://github.com/dominickp/dom-deno" target="_blank">
+          Source on GitHub
+        </a>
+      </span>
     </div>
   );
 }
