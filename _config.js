bd71c10dd303e2776525464d7e4a01b20bb36436
Dom <peluso.dominick@gmail.com>
2026-02-14 12:05:41 -0500

    Update _config.js

--- _config.js
+++ _config.js
@@ -34,6 +34,16 @@ site.add('static')
 site.copy('./robots.txt')
 
 // Minify HTML
-site.use(minifyHTML({ options: { keep_spaces_between_attributes: true } }))
+// NOTE: Preserve script tag attributes to maintain Cloudflare beacon SRI integrity validation
+// Cloudflare injects a beacon script with SRI hash - if minification modifies the script tag
+// structure/attributes, the hash validation fails and the script is blocked by the browser
+site.use(minifyHTML({ 
+    options: { 
+        keep_spaces_between_attributes: true,
+        keep_ssi_data_attributes: true,
+        preserve_entities: true,
+        remove_empty_attributes: false,
+    } 
+}))
 
 export default site
